947a54b67752292407424f714bea00869d8f3c53
hohskiller <kuskild123@gmail.com>
2022-02-20 18:23:58 +0500

    dropdown menu

--- #src/js/menu_burger.js
+++ #src/js/menu_burger.js
@@ -28,10 +28,10 @@ class DynamicAdapt {
 
     // массив уникальных медиа-запросов
     this.mediaQueries = this.оbjects
-      .map(({
-        breakpoint
-      }) => `(${this.type}-width: ${breakpoint}px),${breakpoint}`)
-      .filter((item, index, self) => self.indexOf(item) === index);
+    .map(({
+      breakpoint
+    }) => `(${this.type}-width: ${breakpoint}px),${breakpoint}`)
+    .filter((item, index, self) => self.indexOf(item) === index);
 
     // навешивание слушателя на медиа-запрос
     // и вызов обработчика при первом запуске
@@ -45,7 +45,7 @@ class DynamicAdapt {
         ({
           breakpoint
         }) => breakpoint === mediaBreakpoint
-      );
+        );
       matchMedia.addEventListener('change', () => {
         this.mediaHandler(matchMedia, оbjectsFilter);
       });
@@ -67,7 +67,7 @@ class DynamicAdapt {
             this.moveBack(parent, element, index);
           }
         }
-      );
+        );
     }
   }
 
@@ -140,20 +140,31 @@ class DynamicAdapt {
     }
   }
 }
+let mobile;
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  mobile = true;
+  document.body.classList.add('_mobile');
+} else {
+  document.body.classList.add('_desktop');
+  mobile = false;
+}
 const da = new DynamicAdapt("max");  
 da.init();
 
 
 const header = document.querySelector('.header'),
-	search_hide_items = document.querySelectorAll('.h_search_hide'),
-	header__search = header.querySelector('.header__search')
+search_hide_items = document.querySelectorAll('.h_search_hide'),
+header__search = header.querySelector('.header__search')
 
 if(header) {
 	let delay = 0;
 	search_hide_items.forEach(i => {
-		i.style.transitionDelay = `${delay}s`
-		delay += 0.05;
-	})
+    if(i.classList.contains('header__icon_search') && mobile) {
+      return
+    }
+    i.style.transitionDelay = `${delay}s`
+    delay += 0.05;
+  })
 }
 let header_search_open = false;
 function header_search( action ) {
@@ -186,4 +197,21 @@ document.addEventListener('click', function(e) {
 	if(event.classList.contains('burger')) {
 		document.body.classList.toggle('_burger_open')
 	}
+})
+
+const vh_50_height = window.screen.height / 2;
+let header_fixed = false;
+let header_anime = 400;
+window.addEventListener('scroll', function() {
+  if(window.pageYOffset >= vh_50_height && header_fixed === false) {
+    header_fixed = true;
+    header.classList.add('_fixed');
+  } else if (window.pageYOffset < vh_50_height && header_fixed === true) {
+    header_fixed = false;
+    header.classList.add('_hide');
+    setTimeout(() => {
+      header.classList.remove('_hide');
+      header.classList.remove('_fixed');
+   }, header_anime)
+  }
 })
